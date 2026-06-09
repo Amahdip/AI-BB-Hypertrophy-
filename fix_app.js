fafcc6485d8882c5294087aa1ad843fa86ed3caa
Amirmahdi <amp@Amirmahdis-MacBook-Pro.local>
2026-06-09 14:57:09 +0330

    chore: remove temporary migration and fix scripts

--- fix_app.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const content = fs.readFileSync('src/App.jsx', 'utf8');
-
-const lines = content.split('\n');
-
-const startIndex = lines.findIndex(l => l.includes("experienceLevel === 'beginner' ? ("));
-const endIndex = lines.findIndex((l, idx) => idx > startIndex && l.trim() === ")}") + 1; // Wait, there are multiple ")}".
-
-console.log("Start index:", startIndex);
--- scratch.cjs
+++
@@ -1,3 +0,0 @@
-const React = require('react');
-const { Model } = require('react-body-highlighter');
-console.log(Object.keys(require('react-body-highlighter')));
